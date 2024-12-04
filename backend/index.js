d98ccd6c9e98790615fa00895c9ca9dd5947388d
Sahil Sanjay Patil <97878428+SahilsPatil@users.noreply.github.com>
2024-12-04 16:47:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const accidentRoutes = require('./routes/accidentRoutes');
+// const accidentRoutes = require('./routes/accidentRoutes');
 const accidentSummaryRoutes = require('./routes/accidentSummaryRoutes');
 const userRoutes = require('./routes/userRoutes');
 const jobRoutes = require('./routes/jobRoutes');
@@ -45,7 +45,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch((error) => console.log('Error connecting to MongoDB:', error));
 
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/accidents', accidentRoutes);
+// app.use('/api/accidents', accidentRoutes);
 app.use('/api/accident-summary', accidentSummaryRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/jobs', jobRoutes);
