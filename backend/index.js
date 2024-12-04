49fe187a541402ac56c1b6d24ef6bf9a3880826c
Sahil Sanjay Patil <97878428+SahilsPatil@users.noreply.github.com>
2024-12-04 16:43:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,14 +31,14 @@ app.use(urlencodedParser);
 //     next();
 // });
 
-const imageDir = "./accidents/images";
+// const imageDir = "./accidents/images";
 
 // Ensure the directory exists
-if (!fs.existsSync(imageDir)) {
-    fs.mkdirSync(imageDir, { recursive: true });
-}
+// if (!fs.existsSync(imageDir)) {
+//     fs.mkdirSync(imageDir, { recursive: true });
+// }
 // app.use("/images", express.static("./accidents/images"));
-app.use("/images", express.static(imageDir));
+// app.use("/images", express.static(imageDir));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
